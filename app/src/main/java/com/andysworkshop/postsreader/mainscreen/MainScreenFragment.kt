bc9c8cbef4a1a04abea59757b9789da5a5945d57
Andrzej Kuciel <Andrzej.Kuciel@sigma.se>
2021-10-19 14:42:04 +0200

    Implement acquiring RecyclerView by databinding

--- app/src/main/java/com/andysworkshop/postsreader/mainscreen/MainScreenFragment.kt
+++ app/src/main/java/com/andysworkshop/postsreader/mainscreen/MainScreenFragment.kt
@@ -2,15 +2,13 @@ package com.andysworkshop.postsreader.mainscreen
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
-import com.andysworkshop.postsreader.R
+import com.andysworkshop.postsreader.databinding.FragmentMainScreenListBinding
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
 
@@ -19,7 +17,6 @@ import javax.inject.Inject
  */
 class MainScreenFragment : Fragment() {
 
-    private var columnCount = 1
     private val listData : MutableList<PostListData> = mutableListOf()
     private val recyclerViewAdapter = MainScreenRecyclerViewAdapter(listData)
 
@@ -45,21 +42,15 @@ class MainScreenFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_main_screen_list, container, false)
+    ): View {
+        val binding = FragmentMainScreenListBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        view.layoutManager = LinearLayoutManager(context)
+        view.adapter = recyclerViewAdapter
 
         setupDataObserver()
 
-        // Set the adapter
-        if (view is RecyclerView) {
-            with(view) {
-                layoutManager = when {
-                    columnCount <= 1 -> LinearLayoutManager(context)
-                    else -> GridLayoutManager(context, columnCount)
-                }
-                adapter = recyclerViewAdapter
-            }
-        }
         return view
     }
 
